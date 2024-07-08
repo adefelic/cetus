1fcffa71351c19fdf8a2e1d48312a12f9b4ac36f
adefelic <ad.defelice@live.com>
2024-07-08 15:58:48 -0400

    simplify attacks

--- src/assets/attacks.asm
+++ src/assets/attacks.asm
@@ -4,6 +4,6 @@ INCLUDE "src/macros/attack.inc"
 SECTION "Attack Definitions", ROMX
 
 Attacks::
-	dstruct Attack, AttackSway, "Sway", RT_STRESS, 20, RT_NONE, 0 ; this should heal mp too
-	dstruct Attack, AttackDeceive, "Deceive", RT_CONFUSE, 40, RT_NONE, 0
-	dstruct Attack, AttackTrip, "Trip", RT_CONFUSE, 20, RT_CRUSH, 20
+	dstruct Attack, AttackSway, "Sway", 20
+	dstruct Attack, AttackDeceive, "Deceive", 40
+	dstruct Attack, AttackTrip, "Trip", 20
