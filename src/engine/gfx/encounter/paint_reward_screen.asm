830092bf241c8b656e8179bc21b8b1c68e41724a
adefelic <ad.defelice@live.com>
2024-07-23 08:28:22 -0400

    added reward screen placeholder

---
+++ src/engine/gfx/encounter/paint_reward_screen.asm
@@ -0,0 +1,107 @@
+INCLUDE "src/constants/constants.inc"
+INCLUDE "src/constants/gfx_event.inc"
+INCLUDE "src/constants/palette_constants.inc"
+INCLUDE "src/assets/tiles/indices/bg_tiles.inc"
+INCLUDE "src/assets/tiles/indices/scrib.inc"
+
+DEF REWARD_SCREEN_TOP_LEFT EQU rows 5 + cols 2
+DEF REWARD_SCREEN_WIDTH EQU 20 - 4
+
+SECTION "Reward Screen Painting", ROMX
+
+PaintRewardScreen::
+.blank_row0
+	ld d, TILE_MODAL_EMPTY
+	ld hl, wShadowBackgroundTilemap + REWARD_SCREEN_TOP_LEFT
+	ld b, REWARD_SCREEN_WIDTH
+	call PaintTilemapSmall
+	ld e, BG_PALETTE_UI
+	ld hl, wShadowBackgroundTilemapAttrs + REWARD_SCREEN_TOP_LEFT
+	ld b, REWARD_SCREEN_WIDTH
+	call PaintTilemapAttrsSmall
+.blank_row1
+	ld d, TILE_MODAL_EMPTY
+	ld hl, wShadowBackgroundTilemap + REWARD_SCREEN_TOP_LEFT + rows 1
+	ld b, REWARD_SCREEN_WIDTH
+	call PaintTilemapSmall
+	ld e, BG_PALETTE_UI
+	ld hl, wShadowBackgroundTilemapAttrs + REWARD_SCREEN_TOP_LEFT + rows 1
+	ld b, REWARD_SCREEN_WIDTH
+	call PaintTilemapAttrsSmall
+.text
+	ld d, " "
+	ld hl, wShadowBackgroundTilemap + REWARD_SCREEN_TOP_LEFT + rows 2
+	ld b, 2
+	call PaintTilemapSmall
+	ld d, "y"
+	ld hl, wShadowBackgroundTilemap + REWARD_SCREEN_TOP_LEFT + rows 2 + cols 2
+	ld b, 1
+	call PaintTilemapSmall
+	ld d, "o"
+	ld hl, wShadowBackgroundTilemap + REWARD_SCREEN_TOP_LEFT + rows 2 + cols 3
+	ld b, 1
+	call PaintTilemapSmall
+	ld d, "u"
+	ld hl, wShadowBackgroundTilemap + REWARD_SCREEN_TOP_LEFT + rows 2 + cols 4
+	ld b, 1
+	call PaintTilemapSmall
+	ld d, " "
+	ld hl, wShadowBackgroundTilemap + REWARD_SCREEN_TOP_LEFT + rows 2 + cols 5
+	ld b, 1
+	call PaintTilemapSmall
+	ld d, "d"
+	ld hl, wShadowBackgroundTilemap + REWARD_SCREEN_TOP_LEFT + rows 2 + cols 6
+	ld b, 1
+	call PaintTilemapSmall
+	ld d, "i"
+	ld hl, wShadowBackgroundTilemap + REWARD_SCREEN_TOP_LEFT + rows 2 + cols 7
+	ld b, 1
+	call PaintTilemapSmall
+	ld d, "d"
+	ld hl, wShadowBackgroundTilemap + REWARD_SCREEN_TOP_LEFT + rows 2 + cols 8
+	ld b, 1
+	call PaintTilemapSmall
+	ld d, " "
+	ld hl, wShadowBackgroundTilemap + REWARD_SCREEN_TOP_LEFT + rows 2 + cols 9
+	ld b, 1
+	call PaintTilemapSmall
+	ld d, "i"
+	ld hl, wShadowBackgroundTilemap + REWARD_SCREEN_TOP_LEFT + rows 2 + cols 10
+	ld b, 1
+	call PaintTilemapSmall
+	ld d, "t"
+	ld hl, wShadowBackgroundTilemap + REWARD_SCREEN_TOP_LEFT + rows 2 + cols 11
+	ld b, 1
+	call PaintTilemapSmall
+	ld d, "!"
+	ld hl, wShadowBackgroundTilemap + REWARD_SCREEN_TOP_LEFT + rows 2 + cols 12
+	ld b, 1
+	call PaintTilemapSmall
+	ld d, " "
+	ld hl, wShadowBackgroundTilemap + REWARD_SCREEN_TOP_LEFT + rows 2 + cols 13
+	ld b, 5
+	call PaintTilemapSmall
+
+	ld e, BG_PALETTE_UI + OAMF_BANK1
+	ld hl, wShadowBackgroundTilemapAttrs + REWARD_SCREEN_TOP_LEFT + rows 2
+	ld b, REWARD_SCREEN_WIDTH
+	call PaintTilemapAttrsSmall
+.blank_row2
+	ld d, TILE_MODAL_EMPTY
+	ld hl, wShadowBackgroundTilemap + REWARD_SCREEN_TOP_LEFT + rows 3
+	ld b, REWARD_SCREEN_WIDTH
+	call PaintTilemapSmall
+	ld e, BG_PALETTE_UI
+	ld hl, wShadowBackgroundTilemapAttrs + REWARD_SCREEN_TOP_LEFT + rows 3
+	ld b, REWARD_SCREEN_WIDTH
+	call PaintTilemapAttrsSmall
+.blank_row3
+	ld d, TILE_MODAL_EMPTY
+	ld hl, wShadowBackgroundTilemap + REWARD_SCREEN_TOP_LEFT + rows 4
+	ld b, REWARD_SCREEN_WIDTH
+	call PaintTilemapSmall
+	ld e, BG_PALETTE_UI
+	ld hl, wShadowBackgroundTilemapAttrs + REWARD_SCREEN_TOP_LEFT + rows 4
+	ld b, REWARD_SCREEN_WIDTH
+	call PaintTilemapAttrsSmall
+	ret
